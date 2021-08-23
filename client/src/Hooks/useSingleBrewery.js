f63ba28f2ecc534fb216ff6cf2b60fe412f12a47
CarinaLR <carinalr84@gmail.com>
2021-08-23 18:10:44 -0400

    add comments

--- client/src/Hooks/useSingleBrewery.js
+++ client/src/Hooks/useSingleBrewery.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+//custom hooks allow to break down the logic of fetching data and establish them in a reusable way. customs hooks have to call with the word use... first. The hook will fetch data from openBreweryDB calling axios with a specific id, working with fetch syntax (promise base) it is easier to set a cancelationToken to not send extra requests.
 const useSingleBrewery = (id) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
