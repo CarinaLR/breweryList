07c7249ec72fca2db3fa4a3f4aa2ebdab52f4e5e
CarinaLR <carinalr84@gmail.com>
2021-08-24 13:32:21 -0400

    add scss files

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Layout from "./Layout/Layout";
 import "./App.css";
+import "./main.scss";
 
 export default function App() {
   return (
